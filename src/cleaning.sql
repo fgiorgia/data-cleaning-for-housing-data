830b5402f751dc1be0a009f51af888fcaf1b1a29
Giorgia Faedda <g.faedda7@studenti.unica.it>
2022-10-23 19:01:42 +0200

    Add correct dataset and initial loading

--- src/cleaning.sql
+++ src/cleaning.sql
@@ -1,31 +1,66 @@
+-- Check if this is Windows
+select version();
+DECLARE is_windows CONSTANT datatype [ NOT NULL ] [ { DEFAULT | := } initial_value ]
+
+-- Create DB table
+CREATE TABLE "HousingDataRaw"
+(
+	"UniqueID" VARCHAR(250),
+    "ParcelID" VARCHAR(250), 
+    "LandUse" VARCHAR(250), 
+    "PropertyAddress" VARCHAR(250), 
+    "SaleDate" VARCHAR(250), 
+    "SalePricesString" VARCHAR(250), 
+    "LegalReference" VARCHAR(250), 
+    "SoldAsVacant" VARCHAR(250), 
+    "OwnerName" VARCHAR(250), 
+    "OwnerAddress" VARCHAR(250), 
+    "Acreage" VARCHAR(250), 
+    "TaxDistrict" VARCHAR(250), 
+    "LandValue" VARCHAR(250), 
+    "BuildingValue" VARCHAR(250), 
+    "TotalValue" VARCHAR(250), 
+    "YearBuilt" VARCHAR(250), 
+    "Bedrooms" VARCHAR(250), 
+    "FullBath" VARCHAR(250), 
+    "HalfBath"  VARCHAR(250),
+    "DELETE_ME" VARCHAR(10)
+);
+
+-- Load dataset into our table
+\copy "HousingDataRaw" FROM './data/dataset.csv' DELIMITER '|' CSV HEADER;
+
+--Verify if the Dataset works
+-- SELECT "UniqueID ", "ParcelID", "LandUse", "PropertyAddress", "SaleDate", "SalePricesString", "LegalReference", "SoldAsVacant", "OwnerName", "OwnerAddress", "Acreage", "TaxDistrict", "LandValue", "BuildingValue", "TotalValue", "YearBuilt", "Bedrooms", "FullBath", "HalfBath"
+SELECT *
+FROM "HousingDataRaw"
+LIMIT 10;
+
+--Create a new Table to preserve the original one in order to modify the new one
+CREATE TABLE "HousingData" AS
+(
+	SELECT *
+	FROM "HousingDataRaw" AS hdr 
+);
+
 DO $$
 BEGIN 
 	RAISE NOTICE 'Cleaning Nashville Data...';
 END $$;
 
--- Create DB table
-CREATE TABLE persons (
-  id SERIAL,
-  first_name VARCHAR(50),
-  last_name VARCHAR(50),
-  dob DATE,
-  email VARCHAR(255),
-  PRIMARY KEY (id)
-);
-
-\! pwd
-
--- Load dataset into our table
-\copy persons(first_name, last_name, dob, email) FROM './data/dataset.csv' DELIMITER ',' CSV HEADER;
-
--- Check that loading worked
-SELECT * FROM persons;
-
--- Save table back into dataset
-\copy persons(first_name, last_name, dob, email) TO './out/dataset.csv' DELIMITER ',' CSV HEADER;
+-- Cleaning
 
 
+
+
+
+
+
+-- End cleaning
 DO $$
 BEGIN 
 	RAISE NOTICE 'Cleaning Nashville Data complete!';
 END $$;
+
+-- Save table back into dataset
+\copy "HousingData" TO './out/dataset.csv' DELIMITER ',' CSV HEADER;
