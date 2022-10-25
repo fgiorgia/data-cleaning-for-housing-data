fe133ed81e3d906d47e84c9ff92fd91785d7cbc7
Giorgia Faedda <54286293+fgiorgia@users.noreply.github.com>
2022-10-25 20:15:42 +0200

    Update load_csv.py

--- scripts/load_csv.py
+++ scripts/load_csv.py
@@ -7,27 +7,27 @@ def main():
   parser = argparse.ArgumentParser()
 
   # Adding optional argument
-  # --host=localhost --username=postgres --dbname=postgres -f
-  parser.add_argument('-h', '--host')
-  parser.add_argument('-u', '--username')
-  parser.add_argument('-d', '--dbname')
-  parser.add_argument('-f', '--file')
+  parser.add_argument('--host', required=True)
+  parser.add_argument('--port', default=5432)
+  parser.add_argument('--username', required=True)
+  parser.add_argument('--password', required=True)
+  parser.add_argument('--database', required=True)
+  parser.add_argument('--dataset', required=True)
+  parser.add_argument('--tablename', default='dataset')
 
   # Read arguments from command line
   args = parser.parse_args()
 
-  if args.Output:
-      print("Displaying Output as: % s" % args.Output)
   
 def copy_dataset_to_db():
   # Instantiate sqlachemy.create_engine object
-  engine = create_engine('postgresql://postgres:my_password@localhost:5432/iris')
+  engine = create_engine(f'postgresql://{args.username}:{args.password}@{args.host}:{args.port}/{args.database}')
 
   # Create an iterable that reads "chunksize=1000" rows
   # at a time from the CSV file
-  for df in pd.read_csv("iris.csv",names='infer',chunksize=1000):
+  for df in pd.read_csv(args.dataset, names='infer', chunksize=1000):
     df.to_sql(
-      'HousingDataRaw', 
+      args.tablename, 
       engine,
       index=False,
       if_exists='append' # if the table already exists, append this data
