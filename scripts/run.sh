8adccb65fac93680a860ccd007a75171fbe68035
Giorgia Faedda <g.faedda7@studenti.unica.it>
2022-10-29 14:23:56 +0200

    Enable venv

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,7 @@
 #! /bin/env bash
 
 mkdir -p out
+source .venv/bin/activate
 python3 ./scripts/remove_bom.py --input='./data/dataset.csv' --output='./data/dataset_no_bom.csv'
 python3 ./scripts/load_csv.py --host=localhost --username=postgres --dbname=postgres --dataset='./data/dataset_no_bom.csv' --tablename="HousingDataRaw"
 psql --host=localhost --username=postgres --dbname=postgres -f ./src/cleaning.sql
