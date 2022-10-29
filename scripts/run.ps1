25846859cf2b370963ba0eeaa8f54214b8ddb7ad
Giorgia Faedda <g.faedda7@studenti.unica.it>
2022-10-29 14:23:56 +0200

    Enable venv

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -2,6 +2,7 @@
 # Path: .\scripts\run.ps1
 
 mkdir -p out
+.\.venv\Scripts\Activate.ps1
 python3 .\scripts\remove_bom.py --input=.\data\dataset.csv --output='.\data\dataset_no_bom.csv'
 python3 .\scripts\load_csv.py --host=localhost --username=postgres --dbname=postgres --dataset='.\data\dataset_no_bom.csv' --tablename="HousingDataRaw"
 psql --host=localhost --username=postgres --dbname=postgres -f .\src\cleaning.sql
