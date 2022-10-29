6da1b68fe20942fa8262769d84dfe4a5fcbfdcee
Giorgia Faedda <54286293+fgiorgia@users.noreply.github.com>
2022-10-29 14:02:06 +0200

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,5 @@
 #! /bin/env bash
 
-# Install Python deps
-python3 -m pip install pandas sqlalchemy psycopg2
-
 mkdir -p out
 python3 ./scripts/remove_bom.py --input='./data/dataset.csv' --output='./data/dataset_no_bom.csv'
 python3 ./scripts/load_csv.py --host=localhost --username=postgres --dbname=postgres --dataset='./data/dataset_no_bom.csv' --tablename="HousingDataRaw"
