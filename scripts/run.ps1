556b4a747cc1899dd6b9b1355309fb910ac911e8
Giorgia Faedda <54286293+fgiorgia@users.noreply.github.com>
2022-10-29 14:11:32 +0200

    Update run.ps1

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -1,9 +1,6 @@
-# Install Python deps
-python3 -m pip install pandas sqlalchemy psycopg2
+# Set password in PowerShell with: $env:PGPASSWORD = '*********'
+# Path: .\scripts\run.ps1
 
-
-# Remember to set password as $env:PGPASSWORD = '*********'
-# Remember path: .\scripts\run.ps1
 mkdir -p out
 python3 .\scripts\remove_bom.py --input=.\data\dataset.csv --output='.\data\dataset_no_bom.csv'
 python3 .\scripts\load_csv.py --host=localhost --username=postgres --dbname=postgres --dataset='.\data\dataset_no_bom.csv' --tablename="HousingDataRaw"
