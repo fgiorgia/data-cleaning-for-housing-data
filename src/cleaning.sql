c614432acf6bf4eba4a513fc959220cb72f3fc31
Giorgia Faedda <g.faedda7@studenti.unica.it>
2022-10-30 20:25:15 +0100

    Change column names and remove extra spaces

--- src/cleaning.sql
+++ src/cleaning.sql
@@ -4,6 +4,31 @@ This file contains the final result of my cleaning.
 
 **/
 
+-- Functions
+-- Replaces column names to snake case and remove spaces
+CREATE OR REPLACE PROCEDURE snake_case_column_names(my_table varchar) 
+LANGUAGE plpgsql    
+AS $$
+	DECLARE col_name_item varchar(250);
+	DECLARE new_col_name_item varchar(250);
+BEGIN 
+	FOR col_name_item IN
+  	SELECT column_name FROM information_schema.columns
+        WHERE table_name = my_table
+  	LOOP
+	  	new_col_name_item = trim(lower(regexp_replace(col_name_item, '([a-z])([A-Z])', '\1_\2', 'g')));
+	  	IF new_col_name_item != col_name_item THEN
+		   	EXECUTE format(
+			    'ALTER TABLE %I RENAME COLUMN %I TO %I',
+			    my_table,
+			    col_name_item,
+			    new_col_name_item
+		  	);
+		END IF;
+	  	RAISE NOTICE '%.% -> %.%', my_table, col_name_item, my_table, new_col_name_item;
+  	END LOOP;
+END; $$
+
 -- Let the script fail as it encounters an error
 \set ON_ERROR_STOP true
 SET client_encoding TO 'UTF8';
@@ -27,8 +52,29 @@ END $$;
 
 -- Cleaning
 
-ALTER TABLE "HousingData"
-RENAME COLUMN "UniqueID " TO "UniqueID";
+CALL snake_case_column_names('HousingData');
+
+-- Remove extra spaces from text values
+DO $$
+DECLARE col_name_item varchar(250);
+BEGIN 
+	FOR col_name_item IN 
+		SELECT column_name 
+		FROM information_schema.columns
+        WHERE table_name = 'HousingData' AND data_type = 'text'
+	LOOP
+	RAISE NOTICE '%', format(
+		'UPDATE "HousingData" SET %I = regexp_replace(%I,''[ ]{2,}'', '' '', ''g'')',
+		col_name_item,
+		col_name_item
+	);
+	EXECUTE format(
+		'UPDATE "HousingData" SET %I = regexp_replace(%I,''[ ]{2,}'', '' '', ''g'')',
+		col_name_item,
+		col_name_item
+	);
+	END LOOP;
+END $$;
 
 
 
