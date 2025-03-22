3926cd0dfbff68117b27d388af1ffdfdffcd1d49
Giorgia Faedda <giorgiafaedda3@gmail.com>
2025-03-22 14:36:39 +0100

    Remove old scripts

--- scripts/run.ps1
+++
@@ -1,8 +0,0 @@
-# Set password in PowerShell with: $env:PGPASSWORD = '*********'
-# Path: .\scripts\run.ps1
-
-mkdir -p out
-.\.venv\Scripts\Activate.ps1
-python3 .\scripts\remove_bom.py --input=.\data\dataset.csv --output='.\data\dataset_no_bom.csv'
-python3 .\scripts\load_csv.py --host=localhost --username=postgres --dbname=postgres --dataset='.\data\dataset_no_bom.csv' --tablename="HousingDataRaw"
-psql --host=localhost --username=postgres --dbname=postgres -f .\src\cleaning.sql
--- scripts/setup.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/env bash
-
-# Install Python deps in venv
-python -m venv ./.venv
-source .venv/bin/activate
-python3 -m pip install pandas sqlalchemy psycopg2
