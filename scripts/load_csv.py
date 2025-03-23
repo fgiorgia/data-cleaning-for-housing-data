3b68e236ea5c858f7353ce58c44423d108a2c72c
Giorgia Faedda <giorgiafaedda3@gmail.com>
2025-03-23 22:25:15 +0100

    Simplify handling of psql

--- scripts/load_csv.py
+++ scripts/load_csv.py
@@ -1,45 +1,44 @@
 import pandas as pd
 from sqlalchemy import create_engine
 import argparse
-import os
 
-def main():
-  # Initialize parser
-  parser = argparse.ArgumentParser()
+from scripts.config import get_db_config, DBConfig
 
-  # Adding optional argument
-  parser.add_argument('--host', required=True)
-  parser.add_argument('--port', default=5432)
-  parser.add_argument('--username', required=True)
-  parser.add_argument('--dbname', required=True)
-  parser.add_argument('--dataset', required=True)
-  parser.add_argument('--tablename', default='dataset')
 
-  # Read arguments from command line
-  args = parser.parse_args()
-  
-  if (os.environ.get("PGPASSWORD", None) is None):
-    print("Error: Missing Postgres password as env PGPASSWORD")
-    return exit(1)
-
-  copy_dataset_to_db(args)
-
-  
-def copy_dataset_to_db(args):
+def load_csv_to_db(datasetFile: str, table: str, dbConfig: DBConfig):
   # Instantiate sqlachemy.create_engine object
-  engine = create_engine(f'postgresql://{args.username}:{os.environ["PGPASSWORD"]}@{args.host}:{args.port}/{args.dbname}')
+  engine = create_engine(f'postgresql://{dbConfig["username"]}:{dbConfig["password"]}@{dbConfig["hostname"]}:{dbConfig["port"]}/{dbConfig["database"]}')
 
   # Create an iterable that reads "chunksize=1000" rows
   # at a time from the CSV file
-  print('Copying dataset to database...')
-  for df in pd.read_csv(args.dataset, chunksize=1000, encoding="UTF8"):
+  for df in pd.read_csv(datasetFile, chunksize=1000, encoding="UTF8"):
     print('...')
     df.to_sql(
-      args.tablename, 
+      table, 
       engine,
       index=False,
       if_exists='append' # if the table already exists, append this data
     )
+
+
+def main():
+  # # Initialize parser
+  parser = argparse.ArgumentParser()
+
+  # # Adding optional arguments
+  parser.add_argument('--datasetFile', required=True)
+  parser.add_argument('--table', default='dataset')
+
+  # Read arguments from command line
+  args = parser.parse_args()
+  dbConfig = get_db_config()
+  
+  if dbConfig["password"] is None:
+    print("Error: Missing Postgres password, ensure your env is set-up correctly")
+    return exit(1)
+
+  print('Copying dataset to database...')
+  load_csv_to_db(args.datasetFile, args.table, get_db_config())
   print('Complete!')
 
   
